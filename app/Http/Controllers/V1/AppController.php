5accd1e7de6157a44140ff66ca767bec5a918860
“lizhen” <lizhen19900828@163.cpm>
2017-03-30 20:58:13 +0800

    debug

--- app/Http/Controllers/V1/AppController.php
+++ app/Http/Controllers/V1/AppController.php
@@ -9,7 +9,9 @@ class AppController extends BaseController
 {
     public function login(Request $request){
       $code = $request->get("code");
-      $api_url = "https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=".$code."&grant_type=authorization_code";
-      
+      $api_url = "https://api.weixin.qq.com/sns/jscode2session?appid=".env("APPID")."&secret=".env("APPSECRET")."&js_code=".$code."&grant_type=authorization_code";
+
+      $result = wget($api_url);
+      return $result;
     }
 }
